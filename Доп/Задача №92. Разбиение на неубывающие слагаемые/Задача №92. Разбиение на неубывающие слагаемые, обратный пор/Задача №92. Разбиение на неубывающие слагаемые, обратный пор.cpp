2749e2a31b123c6c5336ddc191b7118314cfcfeb
Екатерина Сметанина <kat.smetanina9@gmail.com>
2020-07-02 20:51:03 +1000

    first commit

---
+++ Доп/Задача №92. Разбиение на неубывающие слагаемые/Задача №92. Разбиение на неубывающие слагаемые, обратный пор/Задача №92. Разбиение на неубывающие слагаемые, обратный пор.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int a[100];
+
+void posled(int index, int m, int n) {
+	if (n == 0){
+		for (int i = 0; i < index;i++) 
+			cout << a[i] << ' ' ;
+
+		cout << endl;
+	}
+	else{
+		for (int i = n; i >= m; i--){
+			a[index] = i;
+			posled(index + 1, i, n - i);
+		}
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	posled(0, 1, n);
+}
---
+++ Доп/Задача №96. Самый длинный путь/Задача №96. Самый длинный путь/Задача №96. Самый длинный путь.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#define INF 1000
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int** mass = new int* [n];
+	for (int i = 0; i < n; i++)
+		mass[i] = new int[n];
+
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++) 
+			cin >> mass[i][j];
+
+			if (mass[i][j] < 0)
+				mass[i][j] = INF;
+	}
+
+	for (int k = 0; k < n; k++) {    // main
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (mass[i][j] > mass[i][k] + mass[k][j])
+					mass[i][j] = mass[i][k] + mass[k][j];
+	}
+
+	int dist = 0;
+
+	for (int i = 0; i < n; i++) {  //print
+		for (int j = 0; j < n; j++)
+			if (mass[i][j]<INF && mass[i][j]>dist)
+				dist = mass[i][j];
+	}
+	cout << dist;
+}
